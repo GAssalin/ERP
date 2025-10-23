50ff72a3ed79a56118212a9b2ceb197bf512c5f7
Gustavo Assalin <gustavo.assalin@hotmail.com>
2025-10-23 12:34:12 -0300

    Criacao inicial alguns MS

---
+++ backend/ms-financeiro/ms-financeiro/src/test/java/br/com/erp/ms_financeiro/MsFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erp.ms_financeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ backend/ms-vendas/ms-vendas/src/test/java/br/com/erp/ms_vendas/MsVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erp.ms_vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
